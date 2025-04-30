65b0cc154ce97450ab2bc7c9647ff48b63a028fd
sun.sun.sun.sun.2001@gmail.com <sun.sun.sun.sun.2001@gmail.com>
2025-04-30 23:37:00 +0300

    Создание пользовательского интерфейса для администрирования отзывов

---
+++ TravelApp.Platform/Areas/Admin/Controllers/FeedbackController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TravelApp.API.Models;
+
+namespace TravelApp.Platform.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class FeedbackController : Controller
+    {
+        public async Task<IActionResult> Index()
+            => View(new List<Feedback>());
+    }
+}
