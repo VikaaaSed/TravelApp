6bae3de5982a3fd224f099f41b0bdc555c15ee7d
sun.sun.sun.sun.2001@gmail.com <sun.sun.sun.sun.2001@gmail.com>
2025-05-05 23:44:52 +0300

    Добавление страницы списка пользователей в панель администратора

---
+++ TravelApp.Platform/Areas/Admin/Controllers/UserController .cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TravelApp.API.Models;
+
+namespace TravelApp.Platform.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new List<User>());
+        }
+    }
+}
