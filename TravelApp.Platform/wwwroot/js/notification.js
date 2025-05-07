186e3a259b13146d1173009ac9ba8ce2720d972d
sun.sun.sun.sun.2001@gmail.com <sun.sun.sun.sun.2001@gmail.com>
2025-05-07 23:58:25 +0300

    Добавление уведомления для пользователя

---
+++ TravelApp.Platform/wwwroot/js/notification.js
@@ -0,0 +1,18 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll('.alert').forEach(alertEl => {
+        bootstrap.Alert.getOrCreateInstance(alertEl);
+    });
+    document.querySelectorAll('.alert .btn-close').forEach(btn => {
+        btn.addEventListener('click', () => {
+            const alert = btn.closest('.alert');
+            if (alert) {
+                bootstrap.Alert.getOrCreateInstance(alert).close();
+            }
+        });
+    });
+    setTimeout(() => {
+        document.querySelectorAll('.alert').forEach(alertEl => {
+            bootstrap.Alert.getOrCreateInstance(alertEl).close();
+        });
+    }, 5000);
+});
