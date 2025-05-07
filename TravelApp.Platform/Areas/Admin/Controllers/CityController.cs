aa5d1de4a937faf62e3808f85b4e2dadff98641a
sun.sun.sun.sun.2001@gmail.com <sun.sun.sun.sun.2001@gmail.com>
2025-05-08 00:25:37 +0300

    Добавление уведомлений для администратора на странице редактирования города

--- TravelApp.Platform/Areas/Admin/Controllers/CityController.cs
+++ TravelApp.Platform/Areas/Admin/Controllers/CityController.cs
@@ -36,19 +36,45 @@ namespace TravelApp.Platform.Areas.Admin.Controllers
         public async Task<IActionResult> CreateOrEdit(City city)
         {
             AllCityInformation model = new(city, city.Id != 0 ? await _cityService.GetLocationInCityByCityIdAsync(city.Id) : []);
-            if (!ModelState.IsValid) return View("CreateOrEdit", model);
-
+            if (!ModelState.IsValid)
+            {
+                TempData["Error"] = $"Ой! Произошла ошибка не все обязательные поля заполнены";
+                return View("CreateOrEdit", model);
+            }
             City? c = await _cityService.GetCityByPageNameAsync(city.PageName);
             if (c != null && c.Id != city.Id)
             {
                 ModelState.AddModelError(nameof(city.PageName), "Страница с таким именем уже существует");
+                TempData["Error"] = $"Ой! Произошла ошибка страница с именем {city.PageName} уже существует";
                 return View("CreateOrEdit", model);
             }
 
             if (city.Id == 0)
-                await _cityService.CreateCityAsync(city);
+            {
+                try
+                {
+                    await _cityService.CreateCityAsync(city);
+                    TempData["Success"] = $"Город {city.PageName} успешно добавлен !!!";
+                }
+                catch
+                {
+                    TempData["Error"] = $"Ой! Произошла ошибка при добавлении города {city.PageName}";
+                }
+            }
+
             else
-                await _cityService.UpdateCityAsync(city);
+            {
+                try
+                {
+                    await _cityService.UpdateCityAsync(city);
+                    TempData["Success"] = $"Город {city.PageName} успешно изменен !!!";
+                }
+                catch
+                {
+                    TempData["Error"] = $"Ой! Произошла ошибка при изменении города {city.PageName}";
+                }
+            }
+
             return RedirectToAction("Index");
         }
 
@@ -56,14 +82,31 @@ namespace TravelApp.Platform.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Delete(int id)
         {
-            await _cityService.DeleteCityAsync(id);
+            try
+            {
+                await _cityService.DeleteCityAsync(id);
+                TempData["Success"] = $"Город с id {id} успешно удален !!!";
+            }
+            catch
+            {
+                TempData["Error"] = $"Ой! Произошла ошибка при удалении города {id}";
+            }
 
             return RedirectToAction(nameof(Index));
         }
         [HttpGet]
         public async Task<IActionResult> DeleteLocation(int id)
         {
-            int idCity = await _cityService.DeleteLocationInCityAsync(id);
+            int idCity = 0;
+            try
+            {
+                idCity = await _cityService.DeleteLocationInCityAsync(id);
+                TempData["Success"] = $"Локация с id {id} успешно удалена !!!";
+            }
+            catch
+            {
+                TempData["Error"] = $"Ой! Произошла ошибка при удалении локации {id}";
+            }
             return RedirectToAction(nameof(Edit), new { id = idCity });
         }
     }
